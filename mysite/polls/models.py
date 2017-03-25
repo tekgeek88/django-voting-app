0ee3c727bb747a452bc4b682a953346d721292fd
tekgeek88 <tekgeek88@yahoo.com>
2017-03-25 09:33:35 -0700

    Finnished tutorial part 2

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,24 @@
+import datetime
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils import timezone
 
 # Create your models here.
+@python_2_unicode_compatible #only if you need to support python 2
+class Question(models.Model):
+    def __str__(self):
+        return self.question_text
+
+    #...
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    def __str__(self):
+        return self.choice_text
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
