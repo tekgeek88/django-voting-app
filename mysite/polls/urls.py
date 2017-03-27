f549698fb46cd5a07f4f8b9a2b0db5c94df1b1d9
tekgeek88 <tekgeek88@yahoo.com>
2017-03-26 22:32:57 -0700

    all good up to Test a View tutorial part 5

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -16,7 +16,7 @@ app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
     #url(r'^$', views.index, name='index'),
-url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     # ex: /polls/5/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
